a5002002b3e0f0dcf42ea1a5d2af5e74232c85ac
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-11-05 20:50:57 +0100

    Install tailwind pretter plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 function App() {
-  return <></>
+  return (
+    <main className='mx-auto flex w-full max-w-[540px] flex-col gap-8'>
+      <h1 className='text-center text-heading-m text-grey'>
+        Password Generator
+      </h1>
+
+      <section className='flex bg-dark-grey px-8 py-5'>
+        <h2 className='cursor-pointer text-heading-m opacity-25'>P4$5W0rD!</h2>
+      </section>
+    </main>
+  )
 }
 
 export default App
