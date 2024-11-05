df067d6d8ac089427d0b82af9742163529c36a1c
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-11-05 20:59:37 +0100

    Add password section

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import CopyIcon from './assets/icon-copy.svg?react'
+
 function App() {
   return (
     <main className='mx-auto flex w-full max-w-[540px] flex-col gap-8'>
@@ -5,8 +7,9 @@ function App() {
         Password Generator
       </h1>
 
-      <section className='flex bg-dark-grey px-8 py-5'>
-        <h2 className='cursor-pointer text-heading-m opacity-25'>P4$5W0rD!</h2>
+      <section className='flex items-center justify-between bg-dark-grey px-8 py-5'>
+        <h2 className='cursor-pointer text-heading-l opacity-25'>P4$5W0rD!</h2>
+        <CopyIcon />
       </section>
     </main>
   )
