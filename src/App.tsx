6558e51466b27e5f5e21cc2db401e24baec3d709
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-11-05 21:05:01 +0100

    Wip options section

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,29 @@ function App() {
         Password Generator
       </h1>
 
-      <section className='flex items-center justify-between bg-dark-grey px-8 py-5'>
-        <h2 className='cursor-pointer text-heading-l opacity-25'>P4$5W0rD!</h2>
-        <CopyIcon />
-      </section>
+      <div className='flex flex-col gap-6'>
+        <section className='flex items-center justify-between bg-dark-grey px-8 py-5'>
+          <h2 className='cursor-pointer text-heading-l opacity-25'>
+            P4$5W0rD!
+          </h2>
+          <CopyIcon />
+        </section>
+
+        <section className='bg-dark-grey px-8 pb-8 pt-6'>
+          <div className='flex items-center justify-between'>
+            <h3>Character Length</h3>
+            <span className='text-heading-l text-neon-green'>0</span>
+          </div>
+
+          {/* INPUT SLIDER */}
+
+          {/* OPTIONS */}
+
+          {/* STRENGTH */}
+
+          {/* GENERATE BUTTON */}
+        </section>
+      </div>
     </main>
   )
 }
