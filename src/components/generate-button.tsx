941a236fa697c4fa950772e64d47c7c7908717dc
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-11-08 20:25:03 +0100

    Fix disabled condition

--- src/components/generate-button.tsx
+++ src/components/generate-button.tsx
@@ -2,12 +2,15 @@ import ArrowRightIcon from '@/assets/icon-arrow-right.svg?react'
 import { usePasswordContext } from '@/lib/utils'
 
 export default function GenerateButton() {
-  const { handleGeneratePassword, isAnyOptionToggled } = usePasswordContext()
+  const { handleGeneratePassword, isAnyOptionToggled, passwordLength } =
+    usePasswordContext()
+
+  const isActive = passwordLength > 0 && isAnyOptionToggled
 
   return (
     <button
       onClick={handleGeneratePassword}
-      disabled={!isAnyOptionToggled}
+      disabled={!isActive}
       className='flex h-[3.5rem] w-full items-center justify-center gap-x-4 border-2 border-neon-green bg-neon-green uppercase text-dark-grey transition-colors duration-200 hover:bg-transparent hover:text-neon-green focus-visible:bg-transparent focus-visible:text-neon-green focus-visible:outline-none disabled:cursor-not-allowed disabled:bg-neon-green disabled:text-dark-grey disabled:opacity-50 md:h-[4.0625rem] md:gap-x-6'
     >
       Generate
